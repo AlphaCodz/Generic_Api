72f1fdd7cc5f6553f91317424913afd800b05ff5
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-14 09:13:13 +0100

    Extensions

--- Generic/apps/telemed/helpers/views.py
+++ Generic/apps/telemed/helpers/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 class BaseView(APIView):
     required_post_fields = set()
     
-    def validate(self, request, format=None):
-        for fields in required_post_field:
+    def post(self, request, format=None):
+        for fields in self.required_post_fields:
             if not request.data.get(fields):
                 res = {
                     "code":400,
