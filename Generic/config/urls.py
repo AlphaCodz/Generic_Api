c63d56c41dd4d36b6ba9323be8f82d875621c599
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-14 10:01:56 +0100

    url updated

--- Generic/config/urls.py
+++ Generic/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("patient/", include("telemed.urls"))
+    path("my_patient/", include("telemed.urls"))
 ]
