eb5c3138e495e810118d02dce444bf30bea3cbe6
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-14 09:16:45 +0100

    Extensions updated

--- Generic/config/settings.py
+++ Generic/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    # REST API
+    "rest_framework",
+    
     # Api apps
     "edumeet",
     "Hostels",
