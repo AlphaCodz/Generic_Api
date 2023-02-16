c838fc8c7a0affae614e7914997949b55899ec07
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-16 08:19:33 +0100

    UniQUE removed

--- Generic/apps/Hostels/models.py
+++ Generic/apps/Hostels/models.py
@@ -7,7 +7,7 @@ class Login(models.Model):
         ('FEMALE', 'FEMALE')
     )
     full_name = models.CharField(max_length=350)
-    matric_no = models.CharField(max_length=25, unique=True)
+    matric_no = models.CharField(max_length=25)
     gender = models.CharField(choices=GENDER, max_length=10)
     
     def __str__(self):
