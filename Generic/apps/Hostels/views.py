1936582f753e99196a059448b01ae72fcbb57e66
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-16 13:51:08 +0100

    issue fixed

--- Generic/apps/Hostels/views.py
+++ Generic/apps/Hostels/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .helpers.views import BaseView
 from .models import Login, Hostel
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
