143c04041479b451b41015c6cd9650e7eb3ba187
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-17 07:58:46 +0100

    debug

--- Generic/config/settings.py
+++ Generic/config/settings.py
@@ -23,7 +23,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
